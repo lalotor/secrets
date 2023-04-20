413a1e171ca0e0503709db1c7728db1094fe4403
Elkin Torres <elkin.torres@prodigious.com>
2023-04-19 22:12:10 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ 
+  extended: true 
+}));
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { 
+  secret: process.env.ENCRYPTION_STRING, 
+  encryptedFields: ['password'] 
+});
+
+const User = mongoose.model("User", userSchema);
+
+function findUserByEmail(email) {
+  return User.findOne({ email: email });
+}
+
+app.get("/", async (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", async (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const foundUser = await findUserByEmail(req.body.username);
+  if (foundUser) {
+    console.log("User '" + req.body.username + "' already exists");
+    return;
+  }
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  const newUser = await user.save();
+  if (newUser === user) {
+    res.render("secrets");
+  } else {
+    console.log("Unexpected error");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const foundUser = await findUserByEmail(req.body.username);
+  if (foundUser && foundUser.password === req.body.password) {
+    res.render("secrets");
+  } else {
+    console.log("User '" + req.body.username + "' not found or invalid password");
+  }
+});
+
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+  });
+})
